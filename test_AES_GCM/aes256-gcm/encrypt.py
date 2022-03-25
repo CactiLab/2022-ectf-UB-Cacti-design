602d5899f317ff186cc2fde1e54e11d4f00e5fcd
Zheyuan (Andy) Ma <philoround@gmail.com>
2022-03-24 20:51:29 -0400

    python side encryption in blocks test code

--- test_AES_GCM/aes256-gcm/encrypt.py
+++ test_AES_GCM/aes256-gcm/encrypt.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import os
+import math
 from Crypto.Cipher import AES
 
 AES_KEY_SIZE = 32
 IV_SIZE = 12
-
+BLOCK_SIZE = 8192
 
 def write_key_bin(key, file_name):
     try:
@@ -15,7 +16,7 @@ def write_key_bin(key, file_name):
     except IOError:
         print("Failed to open the file.")
 
-def main ():
+def main():
     iv =  os.urandom(IV_SIZE)
     key = os.urandom(AES_KEY_SIZE)
     write_key_bin(key, 'key.bin')
@@ -27,16 +28,29 @@ def main ():
     print(data[0:2].hex())
     print(data[2:3].hex())
     fw_ciphertext = b''
-    print(len(fw_ciphertext))
-    for x in range(8):
-        partial_data = data[8192 * x: 8192 * (x+1)]
+    tags = b''
+    blocks = math.ceil(len(data) / BLOCK_SIZE)
+    print(blocks)
+    for x in range(blocks):
+        if x == range(blocks)[-1]:
+            partial_data = data[BLOCK_SIZE * x:]
+        else:
+            partial_data = data[BLOCK_SIZE * x: BLOCK_SIZE * (x+1)]
         cipher = AES.new(key, AES.MODE_GCM, nonce=iv)
         fw_partial, tagf = cipher.encrypt_and_digest(partial_data)
+        tags += tagf
         print(fw_partial[0:16].hex())
-        fw_ciphertext = fw_ciphertext + fw_partial  
+        fw_ciphertext += fw_partial
+        
+    # for x in range(8):
+    #     partial_data = data[8192 * x: 8192 * (x+1)]
+    #     cipher = AES.new(key, AES.MODE_GCM, nonce=iv)
+    #     fw_partial, tagf = cipher.encrypt_and_digest(partial_data)
+    #     print(fw_partial[0:16].hex())
+    #     fw_ciphertext = fw_ciphertext + fw_partial
     print(tagf.hex())
     write_key_bin(fw_ciphertext, 'cipher.bin')
-    write_key_bin(tagf, 'tag.bin')
+    write_key_bin(tags, 'tag.bin')
     print(data[0:16].hex())
     print(fw_ciphertext[0:16].hex())
 
