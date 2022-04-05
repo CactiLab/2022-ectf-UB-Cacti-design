ad125cbfcd3a62c71766d10cb1929f513daef0ad
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2022-04-05 13:51:15 +0000

    minor

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -126,17 +126,17 @@ void handle_boot(void)
 void random_generate(uint8_t *challenge)
 {
     // char str[] = "0123456789abcdef";
-    uint32_t seed = SysTickValueGet();
-    srand(seed);
+    // uint32_t seed = SysTickValueGet();
+    // srand(seed);
     for (int i = 0; i < CHALLENGE_SIZE; i++)
     {
         // memcpy(challenge[i], &time, sizeof(uint32_t));
-        challenge[i] = '0' + rand() % 80;
-        // challenge[i] = i;
+        // challenge[i] = '0' + rand() % 80;
+        challenge[i] = i;
     }
-    SysTickDisable();
-    SysTickPeriodSet(SYSTICK_HIGHEST_VALUE);
-    SysTickEnable();
+    // SysTickDisable();
+    // SysTickPeriodSet(SYSTICK_HIGHEST_VALUE);
+    // SysTickEnable();
 }
 #endif
 
